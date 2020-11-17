6475b3d5d4b2f74a9839f2b8f99746c827dc359e
Aaron Suarez <aaron.j.suarez1@gmail.com>
2020-11-16 19:35:07 -0600

    Add Q+A to facts for now

--- facts.php
+++ facts.php
@@ -1,9 +1,63 @@
-<?php 
+<?php
 $page_title = 'Facts';
 $active_page = 'facts';
-require('_header.php'); 
+require('_header.php');
 ?>
 		<section class="red">
 			<h1>#SaveYourLibraries Facts</h1>
+
+			<h2>What is Proposition #2?</h2>
+			<p>
+				<strong>Proposition #2 would gut the New Orleans Public Library’s budget, cutting 40%
+for the next 20 years.</strong> Mayor Cantrell claims this measure would “renew” library
+funding. In reality, prop #2 cuts the millage that funds the library, and redirects
+those funds elsewhere.
+			</p>
+
+			<h2>How much money does Proposition #2 cut?</h2>
+			<p>
+			<strong>The library system would lose $7.5 million in 2021 alone.</strong> The current millage
+rate of 2.58 provides the majority of the library’s funding. Proposition #2 would
+cut this millage to 0.987.
+			</p>
+
+			<h2>Where would these funds go?</h2>
+			<p>
+			<strong>Most of the library’s lost funding would wind up in an “economic development”
+slush fund.</strong> The propositions on the Dec 5 ballot redistribute taxpayer money
+into three new mayor-controlled funds, with little taxpayer accountability.
+			</p>
+
+			<h2>Would the library lose its independence?</h2>
+			<p>
+				<strong>Yes.</strong> Instead of taxpayer money going directly to the library system, proposition
+#2 would funnel reduced funding into a “library and early education fund”
+controlled by the mayor.
+			</p>
+
+			<h2>Will library services be cut? Will branches close?</h2>
+			<p>
+				The library spends 97% of its budget on operating costs most years. There is no
+doubt that a 40% budget cut will mean reductions in services, branches closing,
+and library workers furloughed.
+			</p>
+
+			<h2>Is this related to COVID-19?</h2>
+			<p>
+				<strong>No.</strong> Cuts to the library budget were fi rst proposed in
+2019. This millage isn’t about austerity or unforeseen
+circumstances.
+			</p>
+
+			<h2>What happens if Proposition #2 fails?</h2>
+			<p>
+				If voters reject Proposition #2, the current millage
+would remain in place until it expires at the end
+of 2021. This would aff ord the community an
+opportunity to propose a millage in line with the
+funding voters have previously approved for the
+library.
+			</p>
+			<img src="/img/vote_no.png" alt="Vote No on #2" class="center">
 		</section>
 <?php require('_footer.php'); ?>
