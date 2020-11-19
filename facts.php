e076201653513dc4ea62d3550017223fa318e96d
Geoffrey Gauchet <geoff@zhephree.com>
2020-11-18 19:43:16 -0600

    update fact sheet

--- facts.php
+++ facts.php
@@ -8,56 +8,195 @@ require('_header.php');
 
 			<h2>What is Proposition #2?</h2>
 			<p>
-				<strong>Proposition #2 would gut the New Orleans Public Library’s budget, cutting 40%
-for the next 20 years.</strong> Mayor Cantrell claims this measure would “renew” library
-funding. In reality, prop #2 cuts the millage that funds the library, and redirects
-those funds elsewhere.
+				Proposition #2 is a measure on the December 5, 2020 ballot that would cut the New Orleans Public Library's budget by 40% for the next 20 years.
+			</p>
+			<p>
+				Advocates for prop #2 claim that the measure would “renew” library funding and support early childhood education. But this simply isn’t true. The reality is that prop #2 cuts library funding, allocates a very small amount to early childhood funding, and creates a huge economic development slush fund. Libraries and early childhood education deserve better. 
+			</p>
+			<img src="/img/facts1.png" alt="CURRENT
+			Library Funding: $19 million. 
+
+			UNDER PROP 2 
+			Early Childhood Education: $1.5 million
+			Library Funding: $11.3 million
+			Economic Development Slush Fund: $8.6 million" class="full-width">
+
+			<h2>What exactly does proposition #2 do?</h2>
+			<p>
+				The New Orleans Public Library is funded by property taxes — also known as millages. Prop #2 would affect one millage in particular, which accounts for a little over half of the NOPL budget with a millage rate of 2.58, equivalent to about $10 million in 2020.
+			</p>
+			<p>
+				If proposition #2 were to pass, this particular millage would be eliminated. In its place, prop #2 would create a new “library and early childhood education” fund. This fund would receive a much smaller millage, with a rate of 0.987, or about $3.8 million in 2021.
+			</p>
+			<img src="/img/facts2.png" alt="CURRENT
+			NOPL Direct Funding
+			Millage #1: 2.58 (about $10.0 million)
+			Millage #2: 2.33 (about $9.0 million)
+
+			IF #2 PASSES
+			NOPL Direct Funding
+			Millage #2: 2.33 (about $9.0 million)
+			''Library and Early Childhood Fund'': 0.987 (about $3.8 million)" class="full-width">
+
+			<h2>Do the proposed millage reallocations lower taxes?</h2>
+			<p>
+				No. The three propositions on the December 5 ballot collectively keep current millage rates exactly the same.
 			</p>
 
-			<h2>How much money does Proposition #2 cut?</h2>
+			<h2>So what’s wrong with proposition #2?</h2>
+			<p>We feel there are several ways in which prop #2 works against our community interests.</p>
+
+			<ul>
+				<li>
+					Prop #2 cuts about 40% of the New Orleans Public Library budget for the next 20 years. This represents a massive, disproportionate cut that would result in reduced services, personnel lay-offs and branch closures. New Orleans loves libraries and have voted for this funding repeatedly. Now is not the time to take resources away from underserved communities.
+				</li>
+				<li>
+					The reduced funding doesn’t go directly to the library like the current millage. Instead, the money goes through a “library and early childhood education” fund. Voters do not have adequate information about how this fund would function. It is clear, however, that prop #2 stands to compromise the NOPL’s independence.
+				</li>
+				<li>
+					Most of the millions of taxpayer dollars cut from the library system wind up in an economic development slush fund. Voters don’t have nearly enough information about this fund, and the lack of transparency and accountability should be worrying to all New Orleans citizens.
+				</li>
+				<li>
+					Prop #2 creates a false choice between early childhood education and libraries. We’re all for the proposed $1.5 million for early childhood education, but NOT when $7.5 million is being cut from libraries, which support early childhood education in their own right. 
+				</li>
+			</ul>
+
 			<p>
-			<strong>The library system would lose $7.5 million in 2021 alone.</strong> The current millage
-rate of 2.58 provides the majority of the library’s funding. Proposition #2 would
-cut this millage to 0.987.
+				We’ll go into more detail about each of these points in this factsheet. But here’s the bottom line: prop #2, along with the rest of the measures on the December 5 ballot, is a raw deal for voters. There’s a reason that New Orleans has overwhelmingly supported the current millage twice before. We love our libraries, and know they make for better, stronger communities.
 			</p>
 
-			<h2>Where would these funds go?</h2>
 			<p>
-			<strong>Most of the library’s lost funding would wind up in an “economic development”
-slush fund.</strong> The propositions on the Dec 5 ballot redistribute taxpayer money
-into three new mayor-controlled funds, with little taxpayer accountability.
+				<strong>On December 5, we’re asking voters to continue funding their libraries and to vote NO on proposition #2.</strong>
 			</p>
 
-			<h2>Would the library lose its independence?</h2>
+			<h2>Where does all the money cut from the libraries go?</h2>
 			<p>
-				<strong>Yes.</strong> Instead of taxpayer money going directly to the library system, proposition
-#2 would funnel reduced funding into a “library and early education fund”
-controlled by the mayor.
+				Each of the three propositions on the December 5 ballot would create one new fund. Let’s take a look at the current millages in question, and how money would be allocated to each of these new funds.
+			</p>
+			<img src="/img/facts3.png" alt="CURRENT MILLAGE RATES
+			Public Library (2.58)
+			Housing Improvement (0.91)
+			Traffic Control + Capital Improvements (2.33)
+
+			PROPOSED MILLAGE RATES
+			Library + Early Childhood Education (0.987)
+			Housing and ''Economic Development'' Fund (2.213)
+			Infrastructure & Maintenance Fund (2.619)" class="full-width">
+			<p>
+				There’s a lot happening in this image, but we want to point out the two biggest changes: the huge cut in library funding, and the giant new “economic development” fund.
+			</p>
+			<h2>How exactly would these funds work?</h2>
+			<p>
+				That’s our question! We’ve gone over every proposal in favor of the December 5 propositions we can find, and we are yet to find any details clarifying exactly how these funds would work.
+			</p>
+			<p>
+				Who would control these funds? How would funding decisions be made? What counts as economic development? How do we make sure any projects undertaken are truly in the community’s interests? Will there be community oversight? How will decision-makers be held accountable? What if taxpayer funds fall into the wrong hands? No one seems to know the answers for sure.
+			</p>
+			<p>
+				Advocates for proposition #2 say they have the best of intentions, and we believe them. But the unfortunate reality is that opaque funds like these seem to invariably open the door to shady, back-room dealings. New Orleanians are <a href="https://www.nola.com/news/courts/article_a4844062-22de-11eb-8ded-17190a5a85fa.html" target="_blank" rel="noopener">all too familiar</a> with the opportunistic predators who use private-public partnerships as a means to extract resources from our communities.
 			</p>
 
-			<h2>Will library services be cut? Will branches close?</h2>
+			<h2>Would the NOPL lose its independence?</h2>
 			<p>
-				The library spends 97% of its budget on operating costs most years. There is no
-doubt that a 40% budget cut will mean reductions in services, branches closing,
-and library workers furloughed.
+				We feel that this has to be a concern. Currently, the NOPL receives their funding directly from taxpayers, and their only agenda is to serve the community. 
+			</p>
+			<p>
+				Could the fund created by prop #2 change this? It’s hard to say when we don’t know precisely how the new fund would work. But it’s easy to imagine a scenario where outside parties wind up dictating how the NOPL must spend its money. It’s possible such fears are unfounded, and that these new funds would be sensibly and impartially governed. Without further information, however, voters have no way of knowing that for sure.
+			</p>
+			<p>
+				The reason we have dedicated public money for the NOPL is because libraries are a public good. It's an investment in a public institution which is available to all people. We've seen the devastation that cuts to public goods have had in this state. Dedicated library funding is a guaranteed way to protect the public good. The library having a dedicated millage means that you get to decide how much money the library gets, and it only gets spent by the library. 
 			</p>
 
-			<h2>Is this related to COVID-19?</h2>
+			<h2>How else would these cuts impact the library?</h2>
 			<p>
-				<strong>No.</strong> Cuts to the library budget were fi rst proposed in
-2019. This millage isn’t about austerity or unforeseen
-circumstances.
+				The NOPL hasn’t released a reduced budget based on prop #2 cuts, so we can’t know the immediate impact with certainty.
+			</p>
+			<p>
+				Here’s what we do know: A 40% cut to the NOPL budget is massive. After speaking with many librarians, community organizations and budget experts, we can confidently state that proposition #2 would lead to reductions in services, personnel lay-offs, and branches closing. In recent years, NOPL has spent 97-98% of its operating budget to keep its 15 branches running. Continuing to do so with severely reduced funding is completely unfeasible.
+			</p>
+			<p>
+				Advocates have claimed that prop #2 would result in no tangible changes to library services. But folks familiar with the NOPL budget say this claim is absurd. There can be no doubt that these budget cuts would lead to reduced services and personnel.
 			</p>
 
-			<h2>What happens if Proposition #2 fails?</h2>
+			<h2>Why is it so important to invest in the New Orleans Public Library?</h2>
 			<p>
-				If voters reject Proposition #2, the current millage
-would remain in place until it expires at the end
-of 2021. This would aff ord the community an
-opportunity to propose a millage in line with the
-funding voters have previously approved for the
-library.
+				With its funding, the NOPL runs 15 branches and employs over 200 New Orleans citizens who rent or own homes in Orleans Parish and contribute to our local economy. Since most of the library’s budget is spent on staff, a cut like this will inevitably affect numerous families and communities in New Orleans. 
 			</p>
-			<img src="/img/vote_no.png" alt="Vote No on #2" class="center">
+			<p>
+				What happens when the people who help unemployed workers are unemployed? Not only will cuts threaten to force library staff into unemployment, there will be less help for other workers who need access to internet, computers and digital literacy assistance. On the day-to-day, library staff offer computer assistance for less privileged or tech-savvy patrons, in addition to computers and free internet. 
+			</p>
+			<p>
+				Libraries have become the only place where people can spend time and access resources without purchasing anything. Some functions of the library have been limited by COVID-19, but library staff know the needs of their communities and are working hard to safely offer everything they can to the public, even during these challenging times. Many library users have told staff that the libraries are keeping them sane during quarantines and lockdowns!
+			</p>
+			<p>
+				With adequate funding, libraries can do a lot. They host and run numerous programs that build stronger communities by supporting adult literacy, early childhood education, career development, summer reading, financial and health literacy and the arts.
+			</p>
+			<p>
+				And we haven’t even mentioned the sheer pleasure of having a local library, of seeing your library staff regularly, getting book recommendations and interacting with knowledgeable staff. But this basic need is one that the library staff fulfill for us as well. 
+			</p>
+			<p>
+				Libraries are the buildings and books, but also the people who staff them and they’re a critical resource for everyone.
+			</p>
+
+			<h2>Would prop #2 bring the library’s budget in line with their true expenditures?</h2>
+			<p>
+				No. 
+			</p>
+			<p>
+				Some of the claims being made in favor of prop #2 can be confusing. Some are in grey areas, or are perhaps a matter of perspective.
+			</p>
+			<p>
+				So let us be clear: This one is just plainly false. 
+			</p>
+			<p>
+				Like any well-run department, the NOPL offers services based on their budget. With more funding, NOPL could offer more services and upgrade facilities. Small budget cuts could perhaps be offset with hiring freezes or modest reductions in programming. 
+			</p>
+			<p>
+				But the notion that cutting NOPL’s budget by 40% in any way aligns with their true expenditures could not be further from the truth. In recent years, NOPL has spent 97-98% of its operating budget to keep its 15 branches running. Continuing to do so with severely reduced funding is completely unfeasible.
+			</p>
+
+			<h2>What about the library’s rainy day fund?</h2>
+			<p>
+				Imagine you manage to save a half-year’s salary. Your boss hears about this, and responds by cutting your salary by 40% for the next 20 years. Your savings aren’t going to get you very far!
+			</p>
+			<p>
+				The library currently has a rainy-day fund of about $11 million — equivalent to about six months of funding. This fund does not reflect a budget surplus. NOPL built up this fund as a matter of basic financial prudence, after Hurricane Katrina demonstrated the need for emergency funds.
+			</p>
+			<p>
+				In order to rebuild and renovate several library branches, extend hours, and hire more staff, NOPL requested an additional 2.5 millage from voters in 2015 — which the community overwhelmingly supported. NOPL finally returned to pre-Katrina levels of services when the Nora Navra branch opened in 2018. With 15 branches, longer hours, more staff and programming, NOPL had its highest circulation numbers ever pre-COVID in 2019.
+			</p>
+
+			<h2>So what about funding early childhood education?</h2>
+			<p>
+				We’re all for it! But proposition #2 isn’t the way to do it. For every $5 cut from the NOPL budget, only $1 goes to early childhood education. The vast majority of it winds up in an “economic development” slush fund.
+			</p>
+			<p>
+				We’d like to see even stronger funding for early childhood education on future ballot measures, while continuing to fund our libraries.
+			</p>
+			<img src="/img/facts4.png" alt="''Every $5 the library is losing, early childhood education only gets $1. So we're devastating our library system to give pennies to early childhood education. It just makes no sense. We're doing five times the harm as we are good.'' -- Andrea Neighbors, Library Board Member" class="full-width">
+
+			<h2>What happens if proposition #2 doesn’t pass?</h2>
+			<p>
+				The current library millage runs through the end of 2021 — giving us over a year to pass a better alternative.
+			</p>
+			<p>
+				The Save Our Libraries, Vote NO on #2 coalition is committed to obtaining a better outcome for our libraries and for our community. If the voters reject proposition #2, we plan to continue the fight, and work together with early childhood education advocates and a broad coalition of stakeholders to come up with a plan that will continue funding the NOPL.
+			</p>
+
+			<h2>Is this hard to understand, or is it just me?</h2>
+			<p>
+				It’s not just you! There’s a lot of moving pieces to this. Ballot language on amendments and propositions can feel intentionally hard to understand, and it doesn’t help that some have tried to frame prop #2 as a vote FOR libraries. 
+			</p>
+
+			<p>
+				If we haven’t answered your question, please reach out to us at <a href="mailto:SaveYourNOLALibrary@gmail.com">SaveYourNOLALibrary@gmail.com</a>, or on our <a href="https://www.facebook.com/SaveYourNOLALibrary" target="_blank" rel="noopener">Facebook</a>, <a href="https://www.instagram.com/saveyournolalibrary/" target="_blank" rel="noopener">Instagram</a>, or <a href="https://twitter.com/savenolalibrary" target="_blank" rel="noopener">Twitter</a>.
+			</p>
+
+
+
+
+
+
+
 		</section>
 <?php require('_footer.php'); ?>
