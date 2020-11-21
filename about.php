9043a3f719d2f742c1ac9de918c50a1c8effb96d
Geoffrey Gauchet <geoff@zhephree.com>
2020-11-21 14:53:51 -0600

    add new coalition member logos

--- about.php
+++ about.php
@@ -38,6 +38,15 @@ require('_header.php');
 
 				<div class="tile">
 					<div class="tile-thumb">
+						<img src="/img/logo_compost.jpg" alt="LOGO: Compost NOW">
+					</div>
+					<div class="tile-links">
+						<a href="http://compost-now.org" target="_blank" rel="noopener">Compost NOW</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
 						<img src="/img/logo_dsa.png" alt="LOGO: Democratic Socialists of America - New Orleans">
 					</div>
 					<div class="tile-links">
@@ -47,10 +56,37 @@ require('_header.php');
 
 				<div class="tile">
 					<div class="tile-thumb">
+						<img src="/img/logo_erase.jpg" alt="LOGO: Erase the Board Coalition">
+					</div>
+					<div class="tile-links">
+						<a href="https://www.facebook.com/erasetheboardnola/" target="_blank" rel="noopener">Erase the Board Coalition</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
 						<img src="/img/logo_fnopl.png" alt="LOGO: Friends of the New Orleans Public Library">
 					</div>
 					<div class="tile-links">
 						<a href="https://friendsnola.org" target="_blank" rel="noopener">Friends of the New Orleans Public Library</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
+						<!-- <img src="/img/logo_fnopl.png" alt="LOGO: Friends of the New Orleans Public Library"> -->
+					</div>
+					<div class="tile-links">
+						<a href="http://www.gnoarchivists.org/" target="_blank" rel="noopener">Greater New Orleans Archivists</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
+						<img src="/img/logo_jewishvoice.png" alt="LOGO: Jewish Voice for Peace NOLA">
+					</div>
+					<div class="tile-links">
+						<a href="https://www.facebook.com/JVPNOLA/" target="_blank" rel="noopener">Jewish Voice for Peace NOLA</a>
 					</div>
 				</div>
 
@@ -68,7 +104,34 @@ require('_header.php');
 						<img src="/img/logo_la_aflcio.png" alt="LOGO: Louisiana AFLCIO">
 					</div>
 					<div class="tile-links">
-						<a href="https://duckduckgo.com/?q=aflcio+la&t=osx" target="_blank" rel="noopener">Louisiana AFLCIO</a>
+						<a href="https://la.aflcio.org" target="_blank" rel="noopener">Louisiana AFLCIO</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
+						<img src="/img/logo_cityworkers.png" alt="LOGO: New Orleans City Workers Organizing Committee">
+					</div>
+					<div class="tile-links">
+						<a href="https://www.facebook.com/cityworkersnola/" target="_blank" rel="noopener">New Orleans City Workers Organizing Committee</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
+						<img src="/img/logo_noma.png" alt="LOGO: NOMA (National Organization of Minority Architects) Louisiana">
+					</div>
+					<div class="tile-links">
+						<a href="http://www.nomala.org" target="_blank" rel="noopener">NOMA (National Organization of Minority Architects) Louisiana</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
+						<img src="/img/logo_opprc.png" alt="LOGO: Orleans Parish Prison Reform Coalition">
+					</div>
+					<div class="tile-links">
+						<a href="https://opprcnola.org/" target="_blank" rel="noopener">Orleans Parish Prison Reform Coalition</a>
 					</div>
 				</div>
 
@@ -83,10 +146,37 @@ require('_header.php');
 
 				<div class="tile">
 					<div class="tile-thumb">
+						<img src="/img/logo_local1846.png" alt="LOGO: United Brotherhood of Carpenters and Pile Drivers - Local 1846">
+					</div>
+					<div class="tile-links">
+						<a href="http://www.local1846.org" target="_blank" rel="noopener">United Brotherhood of Carpenters and Pile Drivers - Local 1846</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
 						<img src="/img/logo_utno.png" alt="LOGO: United Teachers of New Orleans">
 					</div>
 					<div class="tile-links">
 						<a href="http://utno.la.aft.org" target="_blank" rel="noopener">United Teachers of New Orleans</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
+						<img src="/img/logo_local23.png" alt="LOGO: Unite Here Local 23">
+					</div>
+					<div class="tile-links">
+						<a href="https://www.unitehere23.org" target="_blank" rel="noopener">Unite Here Local 23</a>
+					</div>
+				</div>
+
+				<div class="tile">
+					<div class="tile-thumb">
+						<img src="/img/logo_vayla.png" alt="LOGO: VAYLA">
+					</div>
+					<div class="tile-links">
+						<a href="http://www.vayla-no.org" target="_blank" rel="noopener">VAYLA</a>
 					</div>
 				</div>
 			</div>
