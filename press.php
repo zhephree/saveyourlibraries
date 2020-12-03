66706dddc7cd768373aaefe4271789e5ba618529
Geoffrey Gauchet <geoff@zhephree.com>
2020-12-02 20:22:09 -0600

    add new press links

--- press.php
+++ press.php
@@ -6,23 +6,35 @@ require('_header.php');
 		<section class="red">
 			<h1>#SaveYourLibraries Press</h1>
 			<p>
-				<a href="https://www.bgr.org/wp-content/uploads/BGR_Ballot_CityMillagesInBrief.pdf" target="_blank" rel="noopen">On the Ballot: New Orleans Property Tax Propositions December 5, 2020</a><br>
+				<a href="https://www.theadvocate.com/baton_rouge/opinion/article_3d9a32fe-2dd2-11eb-af6b-d389f3c0c033.html" target="_blank"  rel="noopener">Guest column: Gutting the library budget is an assault on dreams of tomorrow's children</a><br>
+				The Advocate New Orleans | 11/30/2020
+			</p>
+			<p>
+				<a href="https://thelensnola.org/2020/11/23/city-accused-of-misleading-the-public-in-campaign-for-property-tax-proposal/" target="_blank"  rel="noopener">City accused of misleading the public in campaign for property tax proposal</a><br>
+				The Lens | 11/23/2020
+			</p>
+			<p>
+				<a href="http://www.louisianaweekly.com/the-criminal-justice-election-our-recommendations-for-dec-5-2020/" target="_blank"  rel="noopener">The Criminal Justice Election: Our Recommendations for Dec. 5, 2020</a><br>
+				The Louisiana Weekly | 11/23/2020
+			</p>
+			<p>
+				<a href="https://www.bgr.org/wp-content/uploads/BGR_Ballot_CityMillagesInBrief.pdf" target="_blank" rel="noopener">On the Ballot: New Orleans Property Tax Propositions December 5, 2020</a><br>
 				Bureau of Governmental Research (BGR)
 			</p>
 			<p>
-				<a href="https://www.theadvocate.com/baton_rouge/opinion/our_views/article_6fa4992c-2a78-11eb-a1ff-0b3f41325f31.html" target="_blank" rel="noopen">Our Views: Vote no on New Orleans millages, city can come up with a better plan</a><br>
+				<a href="https://www.theadvocate.com/baton_rouge/opinion/our_views/article_6fa4992c-2a78-11eb-a1ff-0b3f41325f31.html" target="_blank" rel="noopener">Our Views: Vote no on New Orleans millages, city can come up with a better plan</a><br>
 				NOLA.com | The Times-Picayune
 			</p>
 			<p>
-				<a href="http://antigravitymagazine.com/feature/voter-education-guide-for-new-orleans-elections-saturday-december-5/" target="_blank" rel="noopen">Voter Education Guide for New Orleans Elections Saturday, December 5</a><br>
+				<a href="http://antigravitymagazine.com/feature/voter-education-guide-for-new-orleans-elections-saturday-december-5/" target="_blank" rel="noopener">Voter Education Guide for New Orleans Elections Saturday, December 5</a><br>
 				Antigravity | 11/20/20
 			</p>
 			<p>
-				<a href="https://uptownmessenger.com/2020/11/viewpoint-protect-our-libraries-by-defeating-proposition-2/" target="_blank" rel="noopen">Viewpoint: Protect our libraries by defeating Proposition 2</a><br>
+				<a href="https://uptownmessenger.com/2020/11/viewpoint-protect-our-libraries-by-defeating-proposition-2/" target="_blank" rel="noopener">Viewpoint: Protect our libraries by defeating Proposition 2</a><br>
 				Uptown Messenger | 11/19/20
 			</p>
 			<p>
-				<a href="https://thelensnola.org/2020/11/11/i-hope-the-voters-kill-it-library-board-member-comes-out-against-cantrell-backed-tax-proposal-on-december-ballot/" target="_blank" rel="noopen">‘I hope the voters kill it:’ Library board member comes out against Cantrell-backed tax proposal on December ballot</a><br>
+				<a href="https://thelensnola.org/2020/11/11/i-hope-the-voters-kill-it-library-board-member-comes-out-against-cantrell-backed-tax-proposal-on-december-ballot/" target="_blank" rel="noopener">‘I hope the voters kill it:’ Library board member comes out against Cantrell-backed tax proposal on December ballot</a><br>
 				The Lens | 11/11/20
 			</p>
 
